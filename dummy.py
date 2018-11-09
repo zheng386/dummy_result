4fba9a6f676b8afc56cefb3df1e6f9683af15079
zheng386 <zheng386@hotmail.com>
2018-11-09 12:02:38 +0900

    first commit

---
+++ dummy.py
@@ -0,0 +1,82 @@
+#coding:utf-8
+import random
+from collections import Counter
+from operator import itemgetter
+
+
+class count:
+    def __init__(self,count):
+        #self.item = item
+        self.count = count
+        self.i = 0
+
+    def show(item):
+        return item.count
+
+    def add(item):
+        item.i = item.i + 1
+        return item.i
+
+
+def get_items(array):
+    item = []
+    for i in range(len(array)):
+        item.append(array[i][1])
+    return item
+
+name_file = open('./voc.names')
+html_file = open('./dummy.html','w')
+line = name_file.readline()
+names = []
+results = []
+
+html_file.write('<!DOCTYPE html><html><head><meta charset="UTF-8"></head><title>Dummy result</title><body>This is a test page.<ul>')
+
+while line:
+	names.append(line.strip())
+	line = name_file.readline()
+
+for i in range(random.randrange(30,40)):    #
+    results.append([random.randrange(1,13),names[random.randrange(len(names))]])
+
+
+#print(results)
+
+results_sort_direc = sorted(results)
+print(results_sort_direc)
+
+counter = Counter(get_items(results))
+
+for word, cnt in counter.most_common():
+    #print(word, cnt)
+    exec("%s = count(%d)" % (word , cnt))
+
+    #name = count(word,cnt)
+#print(name.show())
+
+for i in range(1,13):
+    head = False
+    for direc, item in results_sort_direc:
+        if i == direc and head == False:
+            html_file.write('<li><h4>' + str(direc) +'時方向</h4><input type="checkbox" value="0">' + item)
+            head = True
+            if(eval(item).show()>1):
+                html_file.write(' (' + str(eval(item).add()) + ' of ' + str(eval(item).show()) + ')')
+            html_file.write('<br>')
+            #print(item,eval(item).show())
+        elif i == direc :
+            html_file.write('<input type="checkbox" value="' + item + '">' + item)
+            if(eval(item).show()>1):
+                html_file.write(' (' + str(eval(item).add()) + ' of ' + str(eval(item).show()) + ')')
+            html_file.write('<br>')
+            #print(item,eval(item).show())
+    if head == True:
+        html_file.write('</li>')
+
+
+html_file.write('</ul></body></html>')
+
+name_file.close
+html_file.close
+
+print("Done")
